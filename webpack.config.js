ad755d1bb344476315c59d5e289671ebed6177e0
iamdamba <iamdamba720@gmail.com>
2022-04-06 16:11:34 +0200

    06/04/2022 - 16:11 | Update change: React installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   devtool: "source-map",
@@ -7,17 +8,22 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(ts | tsx)$/i,
+        test: /\.(tsx)$/i,
         use: "ts-loader",
         include: [path.resolve(__dirname, "src")],
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".jsx", ".ts", ".js"],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'template.html')
+    })
+  ]
 };
